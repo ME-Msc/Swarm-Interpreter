b4717f0ee7dbfd0f14fc7ef3540a37a13ba38eaf
ME-Msc <1543669883@qq.com>
2024-01-16 21:43:49 +0800

    add symbol & symbolTable

---
+++ symbols/symbol.py
@@ -0,0 +1,24 @@
+class Symbol(object):
+    def __init__(self, name, type=None):
+        self.name = name
+        self.type = type
+
+
+class VarSymbol(Symbol):
+    def __init__(self, name, type):
+        super().__init__(name, type)
+
+    def __str__(self):
+        return '<{name}:{type}>'.format(name=self.name, type=self.type)
+
+    __repr__ = __str__
+
+
+class BuiltinTypeSymbol(Symbol):
+    def __init__(self, name):
+        super().__init__(name)
+
+    def __str__(self):
+        return self.name
+
+    __repr__ = __str__
---
+++ symbols/symbolTable.py
@@ -0,0 +1,93 @@
+from base.nodeVisitor import NodeVisitor
+from symbols.symbol import *
+
+class SymbolTable(object):
+    def __init__(self):
+        self._symbols = {}
+        self._init_builtins()
+
+    def _init_builtins(self):
+        self.define(BuiltinTypeSymbol('INTEGER'))
+
+    def __str__(self):
+        s = 'Symbols: {symbols}'.format(
+            symbols=[(key, value) if key != 'INTEGER' else (value) for key, value in self._symbols.items()]
+        )
+        return s
+
+    __repr__ = __str__
+
+    def define(self, symbol:Symbol):
+        print('Define: %s' % symbol)
+        if symbol.name == 'INTEGER':
+            self._symbols[symbol.name] = symbol
+        else:
+            self._symbols[symbol.name] = symbol.type
+
+    def lookup(self, name):
+        print('Lookup: %s' % name)
+        symbol = self._symbols.get(name)
+        # 'symbol' is either an instance of the Symbol class or 'None'
+        return symbol
+
+
+class SymbolTableBuilder(NodeVisitor):
+    def __init__(self):
+        self.symtab = SymbolTable()
+
+    # def visit_Block(self, node):
+    #     for declaration in node.declarations:
+    #         self.visit(declaration)
+    #     self.visit(node.compound_statement)
+
+    # def visit_Program(self, node):
+    #     self.visit(node.block)
+
+    def visit_BinOp(self, node):
+        left_type = self.visit(node.left)
+        right_type = self.visit(node.right)
+        if left_type.name == right_type.name:
+            return left_type
+        else:
+            raise Exception("Different types on both sides of BinOp")
+
+    def visit_Num(self, node):
+        return BuiltinTypeSymbol('INTEGER')
+
+    def visit_UnaryOp(self, node):
+        self.visit(node.expr)
+
+    def visit_Compound(self, node):
+        for child in node.children:
+            self.visit(child)
+
+    def visit_NoOp(self, node):
+        pass
+
+    # def visit_VarDecl(self, node):
+    #     type_name = node.type_node.value
+    #     type_symbol = self.symtab.lookup(type_name)
+    #     var_name = node.var_node.value
+    #     var_symbol = VarSymbol(var_name, type_symbol)
+    #     self.symtab.define(var_symbol)
+
+    def visit_Assign(self, node):
+        # 'x = 5', node is '='
+        #     = 
+        #   /   \
+        #  x     5
+        var_name = node.left.value
+        type_symbol = self.symtab.lookup(var_name)
+        if type_symbol is None:
+            type_symbol = self.visit(node.right)
+            var_symbol = VarSymbol(var_name, type_symbol)
+            self.symtab.define(var_symbol)
+
+    def visit_Var(self, node):
+        var_name = node.value
+        var_symbol = self.symtab.lookup(var_name)
+
+        if var_symbol is None:
+            raise NameError(repr(var_name))
+        else:
+            return var_symbol
