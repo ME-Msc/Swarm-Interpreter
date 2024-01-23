97d29d91aba73b6e02f39fe7cfda931782ed8a02
ME-Msc <1543669883@qq.com>
2024-01-23 14:19:12 +0800

    update RESERVED_KEYWORDS and get_next_token() in lexer, in order to create tokens automatically. From now,  when we want to add new token for Swarm, we only need to add token in TokenType without changing eat() and get_next_token().

--- lexer/lexer.py
+++ lexer/lexer.py
@@ -1,13 +1,34 @@
-from lexer.token import *
+from lexer.token import Token, TokenType
+from base.error import LexerError
 
-RESERVED_KEYWORDS = {
-    'Port': Token('PORT', 'PORT'),
-    'Action': Token('ACTION', 'ACTION'),
-    'Agent': Token('AGENT', 'AGENT'),
-    'Behavior': Token('BEHAVIOR', 'BEHAVIOR'),
-    'Task': Token('TASK', 'TASK'),
-    'Main': Token('MAIN', 'MAIN')
-}
+def _build_reserved_keywords():
+    """Build a dictionary of reserved keywords.
+
+    The function relies on the fact that in the TokenType
+    enumeration the beginning of the block of reserved keywords is
+    marked with PROGRAM and the end of the block is marked with
+    the END keyword.
+
+    Result: {
+        'Port': Token('PORT', 'PORT'),
+        'Action': Token('ACTION', 'ACTION'),
+        'Agent': Token('AGENT', 'AGENT'),
+        'Behavior': Token('BEHAVIOR', 'BEHAVIOR'),
+        'Task': Token('TASK', 'TASK'),
+        'Main': Token('MAIN', 'MAIN')
+    }
+    """
+    # enumerations support iteration, in definition order
+    tt_list = list(TokenType)
+    start_index = tt_list.index(TokenType.PORT)
+    end_index = tt_list.index(TokenType.EOF)    # NOT include EOF
+    reserved_keywords = {
+        token_type.value: token_type
+        for token_type in tt_list[start_index:end_index]
+    }
+    return reserved_keywords
+
+RESERVED_KEYWORDS = _build_reserved_keywords()
 
 class Lexer(object):
     def __init__(self, text):
@@ -16,17 +37,30 @@ class Lexer(object):
         # self.pos is an index into self.text
         self.pos = 0
         self.current_char = self.text[self.pos]
+        # token line number and column number
+        self.lineno = 1
+        self.column = 1
 
     def error(self):
-        raise Exception('Invalid character')
+        s = "Lexer error on '{lexeme}' line: {lineno} column: {column}".format(
+            lexeme=self.current_char,
+            lineno=self.lineno,
+            column=self.column,
+        )
+        raise LexerError(message=s)
 
     def advance(self):
         """Advance the `pos` pointer and set the `current_char` variable."""
+        if self.current_char == '\n':
+            self.lineno += 1
+            self.column = 0
+
         self.pos += 1
         if self.pos > len(self.text) - 1:
             self.current_char = None  # Indicates end of input
         else:
             self.current_char = self.text[self.pos]
+            self.column += 1
 
     def peek(self):
         peek_pos = self.pos + 1
@@ -49,12 +83,53 @@ class Lexer(object):
 
     def _id(self):
         """Handle identifiers and reserved keywords"""
-        result = ''
+        # Create a new token with current line and column number
+        token = Token(type=None, value=None, lineno=self.lineno, column=self.column)
+
+        value = ''
         while self.current_char is not None and (self.current_char.isalnum() or self.current_char == '_'):
+            value += self.current_char
+            self.advance()
+
+        token_type = RESERVED_KEYWORDS.get(value.upper())
+        if token_type is None:
+            token.type = TokenType.ID
+            token.value = value
+        else:
+            # reserved keyword
+            token.type = token_type
+            token.value = value.upper()
+
+        return token
+    
+    def number(self):
+        """Return a (multidigit) integer or float consumed from the input."""
+
+        # Create a new token with current line and column number
+        token = Token(type=None, value=None, lineno=self.lineno, column=self.column)
+
+        result = ''
+        while self.current_char is not None and self.current_char.isdigit():
             result += self.current_char
             self.advance()
 
-        token = RESERVED_KEYWORDS.get(result, Token(ID, result))
+        '''
+        if self.current_char == '.':
+            result += self.current_char
+            self.advance()
+
+            while self.current_char is not None and self.current_char.isdigit():
+                result += self.current_char
+                self.advance()
+
+            token.type = TokenType.REAL_CONST
+            token.value = float(result)
+        else:
+            token.type = TokenType.INTEGER_CONST
+            token.value = int(result)
+        '''
+        token.type = TokenType.INTEGER
+        token.value = int(result)
         return token
 
     def get_next_token(self):
@@ -73,7 +148,7 @@ class Lexer(object):
                 return self._id()
 
             if self.current_char.isdigit():
-                return Token(INTEGER, self.integer())
+                return self.number()
             
             # peek() is for '<='
             # if self.current_char == '<' and self.peek() == '=':
@@ -81,51 +156,26 @@ class Lexer(object):
             #     self.advance()
             #     return Token(ASSIGN, '<=')
             
-            if self.current_char == ',':
+            # single-character token
+            try:
+                # get enum member by value, e.g.
+                # TokenType(';') --> TokenType.SEMI
+                token_type = TokenType(self.current_char)
+            except ValueError:
+                # no enum member with value equal to self.current_char
+                self.error()
+            else:
+                # create a token with a single-character lexeme as its value
+                token = Token(
+                    type=token_type,
+                    value=token_type.value,  # e.g. ';', '.', etc
+                    lineno=self.lineno,
+                    column=self.column,
+                )
                 self.advance()
-                return Token(COMMA, ',')
+                return token
 
-            if self.current_char == ':':
-                self.advance()
-                return Token(COLON, ':')
-
-            if self.current_char == ';':
-                self.advance()
-                return Token(SEMI, ';')
-
-            if self.current_char == '=':
-                self.advance()
-                return Token(ASSIGN, '=')
-
-            if self.current_char == '+':
-                self.advance()
-                return Token(PLUS, '+')
-
-            if self.current_char == '-':
-                self.advance()
-                return Token(MINUS, '-')
-
-            if self.current_char == '*':
-                self.advance()
-                return Token(MUL, '*')
-
-            if self.current_char == '/':
-                self.advance()
-                return Token(DIV, '/')
-            
-            if self.current_char == '%':
-                self.advance()
-                return Token(MOD, '%')
-
-            if self.current_char == '(':
-                self.advance()
-                return Token(LPAREN, '(')
-
-            if self.current_char == ')':
-                self.advance()
-                return Token(RPAREN, ')')
-
-            self.error()
-
-        return Token(EOF, None)
+        # EOF (end-of-file) token indicates that there is no more
+        # input left for lexical analysis
+        return Token(type=TokenType.EOF, value=None)
 
