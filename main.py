9dc7ccf27e6b839bf3d6206658ffba8cab2d9ff9
ME-Msc <1543669883@qq.com>
2024-01-23 15:23:58 +0800

    remove while in main

--- main.py
+++ main.py
@@ -7,56 +7,57 @@ from base.error import LexerError, ParserError, SemanticError
 import sys
 
 def main():
-    while True:
-        try:
-            # text = input('swarm > ')
-            text = """
-                Port : 14457
+    try:
+        # text = input('swarm > ')
+        text = """
+            Port : 14457
 
-                Action getGPS(test_param):
-                    a = 1;
-                    b = a + 2
-                
-                Agent testUav():
-                    b = 1 + 2
+            Action getGPS(test_param):
+                a = 1;
+                b = a + 2
+            
+            Agent testUav():
+                b = 1 + 2
 
-                Behavior reach_Behavior():
-                    c = 3 + 3
+            Behavior reach_Behavior():
+                c = 3 + 3
 
-                Task reach_in_order_Task():
-                    d  = 6 + 4 
+            Task reach_in_order_Task():
+                d  = 6 + 4 
 
-                Main :
-                    e = 10 + 5
-            """
-        except EOFError:
-            break
-        if not text:
-            continue
+            Main :
+                e = 10 + 5
+        """
+    except EOFError:
+        sys.exit(1)
+    
+    if not text:
+        print("Text is None.")
+        sys.exit(1)
 
-        lexer = Lexer(text)
-        try:
-            parser = Parser(lexer)
-            tree = parser.parse()
-        except (LexerError, ParserError) as e:
-            print(e.message)
-            sys.exit(1)
+    lexer = Lexer(text)
+    try:
+        parser = Parser(lexer)
+        tree = parser.parse()
+    except (LexerError, ParserError) as e:
+        print(e.message)
+        sys.exit(1)
 
-        
-        semantic_analyzer = SemanticAnalyzer()
-        try:
-            semantic_analyzer.visit(tree)
-        except SemanticError as e:
-            print(e.message)
-            sys.exit(1)
+    
+    semantic_analyzer = SemanticAnalyzer()
+    try:
+        semantic_analyzer.visit(tree)
+    except SemanticError as e:
+        print(e.message)
+        sys.exit(1)
 
-        interpreter = Interpreter(tree)
-        interpreter.interpret()
+    interpreter = Interpreter(tree)
+    interpreter.interpret()
 
-        print('')
-        print('Run-time GLOBAL_MEMORY contents:')
-        for k, v in sorted(interpreter.GLOBAL_MEMORY.items()):
-            print('{} = {}'.format(k, v))
+    print('')
+    print('Run-time GLOBAL_MEMORY contents:')
+    for k, v in sorted(interpreter.GLOBAL_MEMORY.items()):
+        print('{} = {}'.format(k, v))
 
 
 if __name__ == '__main__':
