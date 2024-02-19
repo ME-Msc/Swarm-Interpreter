f84726882d816511f064a52b7b44d92091ce1c16
ME-Msc <1543669883@qq.com>
2024-02-19 13:25:51 +0800

    remove Assing, need to test in semanticAnalyzer

--- parser/operator.py
+++ parser/operator.py
@@ -14,8 +14,8 @@ class BinOp(AST):
         self.token = self.op = op
         self.right = right
 
-class Assign(AST):
-    def __init__(self, left, op, right):
-        self.left = left
-        self.token = self.op = op
-        self.right = right
+# class Assign(AST):
+#     def __init__(self, left, op, right):
+#         self.left = left
+#         self.token = self.op = op
+#         self.right = right
