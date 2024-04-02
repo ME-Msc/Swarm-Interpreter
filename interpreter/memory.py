83f1ba973ac28a062fbe0accf4f698c98884b651
ME-Msc <1543669883@qq.com>
2024-04-02 17:17:27 +0800

    add CallStack Tree structure for each_statement in memory.py. Test by running memory.py

--- interpreter/memory.py
+++ interpreter/memory.py
@@ -1,4 +1,5 @@
 from enum import Enum
+import threading
 
 
 class ARType(Enum):         # Activation Record Type
@@ -49,27 +50,100 @@ class ActivationRecord:
 
 class CallStack:
     def __init__(self):
+        self.name = None
         self._records = []
+        self.parent = None
+        self.children = []
 
     def push(self, ar):
         self._records.append(ar)
 
     def pop(self):
-        return self._records.pop()
+        if len(self._records) == 0:
+            if self.parent is None:
+                raise Exception("CallStack has no parent.")
+            else:
+                self.parent.pop()
+        self._records.pop()
 
     def peek(self):
+        if len(self._records) == 0:
+            if self.parent is None:
+                raise Exception("CallStack has no parent.")
+            else:
+                return self.parent._records[-1]
         return self._records[-1]
     
     def bottom(self):
         return self._records[0]
 
+    def create_child(self, nm):
+        child = CallStack()
+        child.name = nm
+        child.parent = self
+        self.children.append(child)
+        return child
+
+    def back_to_parent(self):
+        if not self.parent:
+            raise MemoryError("Call stack node has no parent.")
+        else:
+            parent = self.parent
+            parent.children.remove(self)
+            return parent
+
     def __str__(self):
-        minus = '-' * 30
-        equal = '=' * 30
-        s = '\n'.join(repr(ar) for ar in reversed(self._records))
-        s = f'{equal}\nCALL STACK\n{minus}\n{s}\n{equal}\n\n'
+        minus = '-' * 60
+        equal = '=' * 60
+        node = self
+        s = '\n'.join(repr(ar) for ar in reversed(node._records))
+        while node.parent is not None:
+            node = node.parent
+            parent_s = '\n'.join(repr(ar) for ar in reversed(node._records))
+            s = f'{s}\n{parent_s}'
+        if self.name is None:
+            s = f'{equal}\nCALL STACK\n{minus}\n{s}\n{equal}\n\n'
+        else:
+            s = f'{equal}\nCALL STACK\t\t\t\tAgent:{self.name}\n{minus}\n{s}\n{equal}\n\n'
         return s
 
     def __repr__(self):
         return self.__str__()
 
+
+if __name__ == '__main__':
+    
+    call_stack = CallStack()
+
+    ar1 = ActivationRecord("AR1", ARType.MAIN, 1)
+    ar1['a'] = 0
+    ar1['b'] = 1
+    call_stack.push(ar1)
+    ar2 = ActivationRecord("AR2", ARType.TASK, 2)
+    ar2['c'] = 2
+    call_stack.push(ar2)
+    print(call_stack)
+
+    parent_call_stack = call_stack
+
+    def agent_work(agent_id, cs:CallStack):
+        # if agent_id==1:
+        ar3 = ActivationRecord("AR2", ARType.BEHAVIOR, 3)
+        ar3['d'] = cs.peek()['c'] + agent_id
+        cs.push(ar3)
+        print(cs)
+        cs.back_to_parent()
+            
+    threads = []
+    for now in range(0, 3):
+        child_call_stack = parent_call_stack.create_child(str(now))
+        thread = threading.Thread(target=agent_work, args=(now, child_call_stack,))
+        threads.append(thread)
+
+    # start all threads
+    for thread in threads:
+        thread.start()
+
+    # wait for all threads finish
+    for thread in threads:
+        thread.join()
