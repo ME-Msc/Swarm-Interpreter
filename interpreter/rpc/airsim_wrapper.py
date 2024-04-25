d3865d7bb7171a60b81d31217f164f2a15a606bd
ME-Msc <1543669883@qq.com>
2024-04-25 17:11:00 +0800

    add mulit-client for airsim, fix the bug of multi-thread in each-statement.

--- interpreter/rpc/airsim_wrapper.py
+++ interpreter/rpc/airsim_wrapper.py
@@ -7,18 +7,19 @@ import airsim
 class AirsimWrapper:
 	def __init__(self):
 		# connect to the AirSim simulator
-		self.client = airsim.MultirotorClient()
-		self.client.confirmConnection()
-		self.client.enableApiControl(True)
+		self.clients = {}
 		self.home = {}
 		self.lock = threading.Lock()
 
 		def set_global_camera():
+			client = airsim.MultirotorClient()
 			global_camera = "GlobalCamera"
-			self.client.enableApiControl(True, global_camera)
-			self.client.takeoffAsync(vehicle_name=global_camera).join()
-			self.client.moveToPositionAsync(0, 0, -20, 10, vehicle_name=global_camera).join()
-			self.client.hoverAsync(global_camera).join()
+			self.clients[global_camera] = client
+			self.clients[global_camera].confirmConnection()
+			self.clients[global_camera].enableApiControl(True)
+			self.clients[global_camera].takeoffAsync().join()
+			self.clients[global_camera].moveToPositionAsync(0, 0, -20, 10).join()
+			self.clients[global_camera].hoverAsync().join()
 
 		set_global_camera()
 		message = (
@@ -33,25 +34,32 @@ class AirsimWrapper:
 	def set_home(self, agents_list: list):
 		group = len(self.home)
 		for i in range(len(agents_list)):
+			client = airsim.MultirotorClient()
+			vhcl_nm = agents_list[i]
+			self.clients[vhcl_nm] = client
+
 			pose = airsim.Pose(airsim.Vector3r(group, i * 2, 0), airsim.to_quaternion(0, 0, 0))
-			self.client.simAddVehicle(agents_list[i], "simpleflight", pose)
-			self.home[agents_list[i]] = pose
-			self.client.enableApiControl(True, agents_list[i])
-		# self.client.armDisarm(True, agents_list[i])
+			self.home[vhcl_nm] = pose
+
+			client.simAddVehicle(vhcl_nm, "simpleflight", pose)
+			client.enableApiControl(True, vhcl_nm)
+			client.armDisarm(True, vhcl_nm)
 		time.sleep(2)
 
 	def takeOff_API(self, *rpc_args, vehicle_name):
-		res = self.client.takeoffAsync(vehicle_name=vehicle_name)
+		client = self.clients[vehicle_name]
+
+		res = client.takeoffAsync(vehicle_name=vehicle_name)
 		self.lock.acquire()
 		res.join()
 		self.lock.release()
 
 	def flyToHeight_API(self, *rpc_args, vehicle_name):
+		client = self.clients[vehicle_name]
+		
 		h = rpc_args[0]
-		pose = self.client.simGetVehiclePose(vehicle_name=vehicle_name)
-		self.lock.acquire()
-		res = self.client.moveToPositionAsync(pose.position.x_val, pose.position.y_val, -h, 10, vehicle_name=vehicle_name)
-		self.lock.release()
+		pose = client.simGetVehiclePose(vehicle_name=vehicle_name)
+		res = client.moveToPositionAsync(pose.position.x_val, pose.position.y_val, -h, 10, vehicle_name=vehicle_name)
 		self.lock.acquire()
 		res.join()
 		self.lock.release()
